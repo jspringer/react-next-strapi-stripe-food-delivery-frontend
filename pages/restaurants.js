260d1fe3bf3af5c07228d6a22f62416a6fb0c4ca
Jason Springer <jsnsprngr@gmail.com>
2019-03-29 13:36:06 -0400

    Edit front page

--- pages/restaurants.js
+++ pages/restaurants.js
@@ -39,7 +39,7 @@ class Restaurants extends React.Component {
       return (
         <>
           <div>
-            <span style={{ fontSize: "1.4rem", fontFamily: "'Gothic A1', Roboto, Helvetica, Arial, 'Open Sans'", fontWeight: "700", marginBottom: "50px" }}>{restaurant.name}</span>
+            <span style={{ fontSize: "1.4rem", fontFamily: "'Gothic A1', Roboto, Helvetica, Arial, 'Open Sans'", fontWeight: "700", marginLeft: "30px", marginBottom: "50px", paddingBottom: "50px" }}>{restaurant.name}</span>
           </div>
           <Row style={{ width: "100%", textAlign: "center", paddingTop: "2px", paddingLeft: "20px" }}>
             <Col xs="9" style={{ padding: 0 }}>
